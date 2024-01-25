62b8a56f8f8160a821bb0a5eca9732cb58f645ee
Alexsandro <34524511+AlexsandroMO@users.noreply.github.com>
2024-01-25 13:02:00 +0000

    start projec

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, url_for, request,send_from_directory
+import os
+#import calccode as CalcCode
+#import numpy as np
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
+
+#@app.route('/game_station')
+#def game_station():
+#  return render_template('game-station.html')
+
+
+
+if __name__ == '__main__':
+  #app.run(host='127.0.0.1', port=5000, debug=True)
+  #app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run()
