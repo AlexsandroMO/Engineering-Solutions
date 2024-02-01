39e1543ddc0dc7091d8a6da4de5c511008f24b9d
Alexsandro Oliveira <sandrobass@hotmail.com>
2024-01-31 23:38:19 -0300

    update

--- rotine/urls.py
+++ rotine/urls.py
@@ -1,14 +1,16 @@
 
 from django.urls import path
-from .views import index, home, isa, calc_tension
+from .views import home, isa, calc_tension, name_inst, data_sheet, construction, open_external_web
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('home', home, name='home'),
+    path('', home, name='home'),
     path('isa', isa, name='isa'),
     path('calc_tension', calc_tension, name='calc_tension'),
-
+    path('name_inst', name_inst, name='name_inst'),
+    path('data_sheet', data_sheet, name='data_sheet'),
+    path('construction', construction, name='construction'),
+    path('open_external_web', open_external_web, name='open_external_web'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rotine/views.py
+++ rotine/views.py
@@ -6,19 +6,46 @@ from django.http import HttpResponse
 #from .forms import ProjectForm, LdProjForm #, SubjectForm, PageTypeForm, DocTypeForm, PageformatForm, DocumentModelForm, EmployeeForm, StatusDocForm, ActionForm #, LdProjForm, CotationForm
 from django.contrib import messages
 
-@login_required
-def index(request):
-    return render(request, 'rotine/index.html')
 
-@login_required
+#@login_required
 def home(request):
     return render(request, 'rotine/home.html')
 
-@login_required
+#@login_required
 def isa(request):
     return render(request, 'rotine/isa5-1.html')
 
-@login_required
+#@login_required
 def calc_tension(request):
     return render(request, 'rotine/calc-tension.html')
 
+#@login_required
+def name_inst(request):
+    return render(request, 'rotine/name-inst.html')
+
+#@login_required
+def data_sheet(request):
+    return render(request, 'rotine/data-sheet.html')
+
+#@login_required
+def construction(request):
+    return render(request, 'rotine/construction.html')
+
+
+def open_external_web(request):
+    if request.method == 'POST':
+        meu_valor = request.POST.get('read')
+        print('>>>>>>>>>>>: ', meu_valor)
+        return render(request, 'rotine/data-sheet.html')
+        
+    return render(request, 'rotine/data-sheet.html')
+
+
+'''
+def open_external_web(request):
+    url_externa = "https://www.alldatasheet.com/view.jsp?Searchword=${text_data_sheet}`, '_blank'"  # Substitua isso pela URL externa que deseja abrir
+    return redirect(url_externa)
+'''
+
+
+ 
